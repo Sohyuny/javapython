dc5e7357169e442b8a0bb17469c848ac1604700e
Sohyuny <non.sohyun@gmail.com>
2022-06-24 13:57:54 +0900

    ex0624

---
+++ python_work/220624/a.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("hello",name)
+    print("welcome to python world")
+    a=10
+    b=20
+    print(a)
+    print(b)
+
+
+greet("sohyun")
+print("aa")
+greet("hyun")
---
+++ python_work/220624/b.py
@@ -0,0 +1,5 @@
+def func(name,age):
+    print("name : ",name)
+    print("age : ",age)
+
+func('sohyun',24)
